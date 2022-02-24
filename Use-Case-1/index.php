9dfa0fcb1b7bf8d97949799698c5c9c78bded918
Ellen Seghers <ellen.seghers1@gmail.com>
2022-02-24 10:02:22 +0100

    "Created Variabele"

---
+++ Use-Case-1/index.php
@@ -0,0 +1,14 @@
+<?php
+
+//Create the variabele for banana's, apple's and bottles of wine
+$numberBanana = 6;
+$priceBanana = 1;
+$numberApple = 3;
+$priceApple = 1.5;
+$numberBottlesOfWine = 2;
+$priceBottleOfWine = 10;
+
+
+
+
+?>
