2ad9d468edd59e7ee1f3b2319f3574f7b7fb70f8
Ellen Seghers <ellen.seghers1@gmail.com>
2022-02-25 12:23:16 +0100

    "Average calculation of students of a group"

--- Use-Case-3/index.php
+++ Use-Case-3/index.php
@@ -1,36 +1,65 @@
 <?php
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 class Student
 {
     private $grade;
     private $name;
     private $group;
 
-    public function __constructor($grade, $name, $group)
+    public function __construct($grade, $name, $group)
     {
         $this->grade = $grade;
         $this->name = $name;
         $this->group = $group;
     }
 
-    public function getScore ()
+    public function getScore()
     {
         return $this->grade;
     }
+
+    public function getGroup()
+    {
+        return $this->group;
+    }
 }
 
-$group1 = [];
+$students = [];
 
-for($i = 0 ; $i < 10 ;$i++)
+for($i = 0 ; $i < 10 ; $i++)
 {
-    $group1[$i] = new Student(0, "A", "group1");
+    $students[$i] = new Student($i, "A", "group1");
 }
 
-$group2 = [];
-
-for($i = 0; $i < 10; $i++)
+for($i = 10 ; $i < 20 ; $i++)
 {
-    $group2[$i] = new Student(0, "A", "Group2");
+    $students[$i] = new Student($i -1, "A", "group2");
 }
+
+$score1 = 0;
+$score2 = 0;
+$count1 = 0;
+$count2 = 0;
+
+for($i = 0 ; $i < sizeof($students) ; $i++)
+{
+    if($students[$i]->getGroup() == "group1")
+    {
+        $score1 += $students[$i]->getScore();
+        $count1++;
+    }
+    elseif($students[$i]->getGroup() == "group2")
+    {
+        $score2 += $students[$i]->getScore();
+        $count2++;
+    }
+}
+print_r($score1 / $count1);
+echo "<br>";
+print_r($score2 / $count2);
 
 ?>
