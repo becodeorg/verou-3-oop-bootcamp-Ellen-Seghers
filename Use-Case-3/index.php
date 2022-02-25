20e3c7cf77618f0a03fd2d5e0ebfdbdae01d43fe
Ellen Seghers <ellen.seghers1@gmail.com>
2022-02-25 11:30:15 +0100

    "Created 20 student objects in 2 groups"

--- Use-Case-3/index.php
+++ Use-Case-3/index.php
@@ -6,7 +6,7 @@ class Student
     private $name;
     private $group;
 
-    public function __constructor ($grade, $name, $group)
+    public function __constructor($grade, $name, $group)
     {
         $this->grade = $grade;
         $this->name = $name;
@@ -19,10 +19,18 @@ class Student
     }
 }
 
+$group1 = [];
+
 for($i = 0 ; $i < 10 ;$i++)
 {
-
+    $group1[$i] = new Student(0, "A", "group1");
 }
 
+$group2 = [];
+
+for($i = 0; $i < 10; $i++)
+{
+    $group2[$i] = new Student(0, "A", "Group2");
+}
 
 ?>
